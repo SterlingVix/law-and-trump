94ede5a0d642cfc6223937ec7b96aeef0096d64c
Aaron Melocik <amelocik@gmail.com>
2017-05-11 14:04:14 -0700

    Cleanup before build.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,3 @@
-// import React, { Component } from 'react';
-// import ReactDOM from 'react-dom';
-//
-// import CardsTable from './components/CardsTable';
-//
-// import './index.css';
-//
-// ReactDOM.render(
-//   <CardsTable />,
-//   document.getElementById('root')
-// );
-
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
